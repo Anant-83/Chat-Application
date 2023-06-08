fbc498da971a5ad87a58757f77d3e7efefec6df6
Anant Prajapati <113686236+Anant-83@users.noreply.github.com>
2023-06-08 19:21:31 +0530

    Delete src directory

--- src/message_storage.js
+++
@@ -1,9 +0,0 @@
-const messages = [];
-
-function savemessage(message) {
-  messages.push(message);
-}
-function findmessageforuser(userId) {
-  return messages.filter(({ from, to }) => from === userId || to === userId);
-}
-module.exports = { savemessage, findmessageforuser, messages };
